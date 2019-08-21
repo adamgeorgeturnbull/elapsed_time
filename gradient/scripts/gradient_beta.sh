9f8de125f334fbaf17452db6014a51a9f8a11e6e
Adam Turnbull <agt520@nidesk410.ynic.york.ac.uk>
2019-08-21 13:11:01 +0100

    re-extracted gradient scores after updating participant 50

--- gradient/scripts/gradient_beta.sh
+++ gradient/scripts/gradient_beta.sh
@@ -8,7 +8,7 @@ set -e # stop if I error
 set -u # error if you call an unset (empty) variable
 
 # Alternatively, you can create a list of zstat maps in a text file and use cat to load it.
-subjdirs=`cat /scratch/groups/Projects/P1300/fMRI_data/elapsed_time/gradient/subject_level/subjectlist_elapsed_time_TT_WM.txt`
+subjdirs=`cat /scratch/groups/Projects/P1300/fMRI_data/elapsed_time/gradient/subject_level/subjectlist_elapsed_time_TT.txt`
 
 # mask to use for correlations
 Mask="/scratch/groups/Projects/P1300/fMRI_data/elapsed_time/gradient/gradient_mask.nii.gz"
@@ -23,10 +23,10 @@ for n in $subjdirs; do
 	echo $n
 
 	fsl_glm -i $n -d $Grad1 -o tmp.txt --demean -m $Mask 
-	cat tmp.txt >> betas_elapsed_timeTT_WM1.txt
+	cat tmp.txt >> betas_elapsed_timeTT1.txt
 	fsl_glm -i $n -d $Grad2 -o tmp.txt --demean -m $Mask
-	cat tmp.txt >> betas_elapsed_timeTT_WM2.txt
+	cat tmp.txt >> betas_elapsed_timeTT2.txt
 	fsl_glm -i $n -d $Grad3 -o tmp.txt --demean -m $Mask
-	cat tmp.txt >> betas_elapsed_timeTT_WM3.txt
+	cat tmp.txt >> betas_elapsed_timeTT3.txt
 done
 
